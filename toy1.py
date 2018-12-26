42ba3bf930be7cc5d1b9f80e6f1461aafb4874f0
Tomoaki Sugiayma <sti320a@gmail.com>
2018-12-27 00:27:10 +0900

    Add calculatin of lst

--- toy1.py
+++ toy1.py
@@ -1,6 +1,10 @@
-import os
-import sys
-import flask
+nums = [1, 2, 3]
 
-print('Hello, flask')
-print('Hello, flask')
+nums += [4]  # [1, 2, 3] + [4] = [1, 2, 3, 4]
+print(nums)
+
+nums.append([5]) # [1, 2, 3, 4] append [5] = [1, 2, 3, 4, [5]]
+print(nums)
+
+nums.append(6) # [1, 2, 3, 4, [5]] append 6 = [1, 2, 3, 4, [5], 6]
+print(nums)
